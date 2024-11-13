858d33c292a6af500abd7d018e313bc7d5e6f728
griffinjoshs <leveloutusa@gmail.com>
2024-11-13 01:21:04 -0500

    animation projects

--- gws_react/src/themes/HighTechNeon/StaggeredAnimation.js
+++ gws_react/src/themes/HighTechNeon/StaggeredAnimation.js
@@ -1,11 +1,25 @@
 // StaggeredAnimation.js
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 
 const StaggeredAnimation = ({ children, index, delayBase = 100 }) => {
-  const calculatedDelay = index * delayBase;
+  const [shouldStagger, setShouldStagger] = useState(window.innerWidth >= 768);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setShouldStagger(window.innerWidth >= 768);
+    };
+
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  const calculatedDelay = shouldStagger ? index * delayBase : 0;
+
   return React.cloneElement(children, {
-    style: { ...children.props.style, '--delay-in': `${calculatedDelay / 1000}s` }
+    style: { ...children.props.style, '--delay-in': `${calculatedDelay / 1000}s` },
   });
 };
 
